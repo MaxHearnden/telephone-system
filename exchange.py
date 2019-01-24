0a7c2c3c90510b6c02d95f7a6cb77d59cb8be6ed
MaxHearnden <max@hearnden.org.uk>
2019-01-24 16:39:37 +0000

    started the crossbar exchange code

---
+++ exchange.py
@@ -0,0 +1,8 @@
+class bar:
+    def __init__(self,perm,ident):
+        self.perm=list(perm)
+        self.ident=ident
+        self.con=[]
+        return self
+    def data(self,source,data):
+        
